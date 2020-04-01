5244016ec4bcbb021f6de47428d753f97d51e484
AndriyUstinskyy <44446040+AndriyUstinskyy@users.noreply.github.com>
2020-04-01 14:39:08 +0300

    Update CityFragment.java

--- CityFragment.java
+++ CityFragment.java
@@ -9,29 +9,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CityFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
 public class CityFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
     public CityFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_city, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -55,18 +46,8 @@ public class CityFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+  
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
